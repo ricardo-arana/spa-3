846941fcb01a7fa920fef467b92a4df26d0445bd
Ricardo Arana Reyes <raranare@everis.com>
2020-12-14 12:31:15 -0500

    router

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from 'src/app/models/heroe.model';
 import { HeroeService } from 'src/app/services/heroe.service';
 
@@ -10,7 +10,9 @@ import { HeroeService } from 'src/app/services/heroe.service';
 })
 export class DetalleComponent implements OnInit {
   heroe: Heroe;
-  constructor(private activatedRoute: ActivatedRoute, private heroeService: HeroeService) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private heroeService: HeroeService,
+              private router: Router) {
     this.activatedRoute.params.subscribe( parms => {
       this.heroe = this.heroeService.obtenerHeore(+parms['id'].toString())
       console.log(this.heroe);
@@ -18,6 +20,12 @@ export class DetalleComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  regresar() {
+    console.log('El usuario regreso a galeria');
+    // this.router.navigate(['/galeria']);
+    this.router.navigateByUrl('/galeria')
   }
 
 }
